7f8085e229a961c6633d640b3d45d527b8620c9a
unknown <kevinliu8260@hotmail.com>
2021-05-24 01:36:37 -0700

    Cleaned coded & Implemented add & del row functions

--- src/main/resources/public/javascript/calculator.js
+++ src/main/resources/public/javascript/calculator.js
@@ -1,4 +1,3 @@
-
 function calcMean() {
     var gradeScore = document.getElementsByClassName("score");
     var gradeTotal = document.getElementsByClassName("total");
@@ -9,7 +8,6 @@ function calcMean() {
     var percent = 0;
     var result = 0;
     var count = 0;
-
     var length = gradeScore.length;
     
     for (var j = 0; j < length; j++) {
@@ -18,12 +16,9 @@ function calcMean() {
         } else if (gradeScore[j].value != 0 && gradeTotal[j].value != 0) {
             count += 1;
         } else if (gradeTotal[j].value == 0) {
-            console.log("Error");
+            count += 0;
         }
     }
-
-    console.log("length: " + gradeTotal.length);
-    console.log("length2: " + count);
 
     for (var i = 0; i < length; i++) {
         if (gradeScore[i].value == 0 && gradeTotal[i].value > 0) {
@@ -32,16 +27,17 @@ function calcMean() {
         } else if (gradeScore[i].value != 0 && gradeTotal[i].value != 0) {
             percent = gradeScore[i].value/gradeTotal[i].value;
             sum += percent;
-        } else if (gradeTotal[i].value == 0) {
-            console.log("Error");
+        }
+        
+        if (gradeScore[i].value != null && gradeTotal[i].value == null) {
+            break;
+        } else if (gradeScore[i].value != null && gradeTotal[i].value == "0") {
+            alert("Grades cannot be out of zero!");
         }
     }
 
     result = sum/count;
     result *= 100;
-
-    console.log("result: " + result);
-
     calcResult.innerHTML = "The Mean (average) is: " + result + "%";
 }
 
@@ -65,10 +61,9 @@ function calcWeighted() {
         } else if (gradeScore[j].value != 0 && gradeTotal[j].value != 0) {
             count += 1;
         } else if (gradeTotal[j].value == 0) {
-            console.log("Error");
+            count += 0;
         }
     }
-
 
     for (var i = 0; i < length; i++) {
         if (gradeScore[i].value == 0 && gradeTotal[i].value > 0) {
@@ -79,17 +74,17 @@ function calcWeighted() {
             percent = gradeScore[i].value/gradeTotal[i].value;
             weightTotal += parseFloat(weightScore[i].value);
             sum += percent*weightScore[i].value;
-        } else if (gradeTotal[i].value == 0) {
-            console.log("Error");
+        }
+
+        if (gradeScore[i].value != null && gradeTotal[i].value == null) {
+            break;
+        } else if (gradeScore[i].value != null && gradeTotal[i].value == "0") {
+            alert("Grades cannot be out of zero!");
         }
     }
 
     result = sum/weightTotal;
     result *= 100;
-
-    console.log("WT: " + weightTotal);
-    console.log("sum: "+ sum);
-    console.log("result: " + result);
 
     calcResult.innerHTML = "The Weighted Average is: " + result + "%";
 }
@@ -109,4 +104,73 @@ function updatePercent() {
 function isValid(evt){
     var charCode = (evt.which) ? evt.which : event.keyCode;
     return !(charCode > 31 && (charCode < 48 || charCode > 57) && charCode != 46);
+}
+
+function addRow() {
+    var table = document.getElementById("table");
+    var lastRow = document.getElementById("table").rows.length;
+    var row = table.insertRow(lastRow);
+
+    var newName = row.insertCell(0);
+    var newShortName = row.insertCell(1);
+    var newWeight = row.insertCell(2);
+    var newGrade = row.insertCell(3);
+    var newPercent = row.insertCell(4);
+
+    var nameInput = document.createElement("INPUT");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("class", "newName");
+    nameInput.setAttribute("placeholder", "Enter Name");
+    newName.appendChild(nameInput);
+
+    var shortNameInput = document.createElement("INPUT");
+    shortNameInput.setAttribute("type", "text");
+    shortNameInput.setAttribute("class", "newShortName");
+    shortNameInput.setAttribute("placeholder", "Enter Short Name");
+    newShortName.appendChild(shortNameInput);
+
+    var weightInput = document.createElement("INPUT");
+    weightInput.setAttribute("type", "number");
+    weightInput.setAttribute("class", "weight");
+    weightInput.setAttribute("min", "0");
+    weightInput.setAttribute("onkeypress", "return isValid(event)");
+    newWeight.appendChild(weightInput);
+
+    var gradeScoreInput = document.createElement("INPUT");
+    var gradeTotalInput = document.createElement("INPUT");
+
+    gradeScoreInput.setAttribute("type", "number");
+    gradeScoreInput.setAttribute("class", "score");
+    gradeScoreInput.setAttribute("oninput", "updatePercent()");
+    gradeScoreInput.setAttribute("min", "0");
+    gradeScoreInput.setAttribute("onkeypress", "return isValid(event)");
+
+    var slashImg = document.createElement("IMG");
+
+    slashImg.setAttribute("src", "https://static.thenounproject.com/png/2342342-200.png")
+    slashImg.setAttribute("width", "25");
+    slashImg.setAttribute("height", "25");
+    slashImg.setAttribute("class", "slash");
+    slashImg.setAttribute("alt", "forward slash");
+
+    gradeTotalInput.setAttribute("type", "number");
+    gradeTotalInput.setAttribute("class", "total");
+    gradeTotalInput.setAttribute("oninput", "updatePercent()");
+    gradeTotalInput.setAttribute("min", "0");
+    gradeTotalInput.setAttribute("onkeypress", "return isValid(event)");
+
+    newGrade.appendChild(gradeScoreInput);
+    newGrade.appendChild(slashImg);
+    newGrade.appendChild(gradeTotalInput);
+
+    newPercent.setAttribute("class", "percent");
+    newPercent.innerHTML = "";
+}
+
+function delRow() {
+    var lastRow = document.getElementById("table").rows.length;
+    
+    if (lastRow != 1) {
+        document.getElementById("table").deleteRow(lastRow-1);
+    }
 }
